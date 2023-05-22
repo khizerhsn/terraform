429e68145ab5af87c97cf37d616ba4e612581cb8
khizerhsn <50544149+khizerhsn@users.noreply.github.com>
2023-05-22 15:19:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,11 @@
 provider "aws" {
- region = "ap-south-1"
+  access_key = "AKIAZH76EJCHRLG3O2OF"
+  secret_key = "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"
+  region     = "ap-south-1"
 }
 
-resource "aws_iam_role" "nonprod_role" {
- name               = "nonprod-role"
+resource "aws_iam_role" "AWSServiceRoleForAmazonEMRServerless" {
+ name               = "AWSServiceRoleForAmazonEMRServerless"
  assume_role_policy = <<EOF
 {
  "Version": "2012-10-17",
@@ -19,8 +21,8 @@ resource "aws_iam_role" "nonprod_role" {
 EOF
 }
 
-resource "aws_iam_policy" "nonprod_policy" {
- name        = "nonprod-policy"
+resource "aws_iam_policy" "AWSServiceRoleForAmazonEMRServerless" {
+ name        = "AWSServiceRoleForAmazonEMRServerless"
  description = "Policy for EMR Studio"
 
  policy = <<EOF
@@ -44,65 +46,65 @@ EOF
 }
 
 resource "aws_iam_role_policy_attachment" "emr_studio_role_policy_attachment" {
- role       = aws_iam_role.nonprod_role.name
- policy_arn = aws_iam_policy.nonprod_policy.arn
+ role       = aws_iam_role.AWSServiceRoleForAmazonEMRServerless.name
+ policy_arn = aws_iam_policy.AWSServiceRoleForAmazonEMRServerless.arn
 }
 
-resource "aws_emr_studio" "uws-emrserverless-studio-nonprod" {
+resource "aws_emr_studio" "emrserverless-studio" {
  auth_mode                   = "IAM"
- default_s3_location         = "s3://cloudops-emrserverless/emr-serverless/"
- engine_security_group_id    = "sg-077a2d1f378103407"
- name                        = "uws-emrserverless-studio-nonprod"
- service_role                = aws_iam_role.nonprod_role.arn
- subnet_ids                  = ["subnet-0091d45f89f2e267b", "subnet-01fe66d8d5b0efb18"]
- vpc_id                      = "vpc-006438faf6005e56c"
- workspace_security_group_id = "sg-0e63af6afb024313a"
+ default_s3_location         = "s3://khizer-emr/emr/"
+ engine_security_group_id    = "sg-0e1990bee4e65356f"
+ name                        = "uws-emrserverless-studio"
+ service_role                = aws_iam_role.AWSServiceRoleForAmazonEMRServerless.arn
+ subnet_ids                  = ["subnet-d8ecbfb0", "subnet-525ede1e"]
+ vpc_id                      = "vpc-105d6878"
+ workspace_security_group_id = "sg-0970e3e1591fb551d"
 }
 
-resource "aws_emrserverless_application" "click_log_loggregator_emr_serverless" {
- name          = "uws-testing-application"
- release_label = "emr-6.9.0"
+resource "aws_emrserverless_application" "emr_serverless" {
+ name          = "emr_serverless"
+ release_label = "emr-6.8.0"
  type          = "spark"
 
- initial_capacity {
-   initial_capacity_type = "Driver"
+ 
+  initial_capacity = {
+    driver = {
+      initial_capacity_type = "Driver"
 
-   initial_capacity_config {
-     worker_count = 1
-     worker_configuration {
-       cpu    = "4 vCPU"
-       memory = "20 GB"
-     }
-   }
- }
+      initial_capacity_config = {
+        worker_count = 2
+        worker_configuration = {
+          cpu    = "4 vCPU"
+          memory = "12 GB"
+        }
+      }
+    }
 
- initial_capacity {
-   initial_capacity_type = "Executor"
+    executor = {
+      initial_capacity_type = "Executor"
 
-   initial_capacity_config {
-     worker_count = 3
-     worker_configuration {
-       cpu    = "4 vCPU"
-       memory = "20 GB"
-     }
-   }
- }
+      initial_capacity_config = {
+        worker_count = 2
+        worker_configuration = {
+          cpu    = "8 vCPU"
+          disk   = "64 GB"
+          memory = "24 GB"
+        }
+      }
+    }
+  }
 
- maximum_capacity {
-   cpu    = "2000 vCPU"
-   memory = "10000 GB"
- }
-
+  maximum_capacity = {
+    cpu    = "48 vCPU"
+    memory = "144 GB"
+  }
+  
+  network_configuration = {
+    subnet_ids = ["subnet-d8ecbfb0", "subnet-525ede1e"]
+  }
+  
  tags = {
-   application-name = "uws"
+   application-name = "test"
    environment-type = "non-prod"
- }
-}
-
-terraform {
- backend "s3" {
-   bucket = "cloudops-emrserverless"
-   key    = "EMR-DevOps/terraform.tfstate"
-   region = "ap-south-1"
  }
 }
